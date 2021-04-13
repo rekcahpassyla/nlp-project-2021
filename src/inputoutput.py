7170438cd119285e1a42a514919ee2dd51d8a4e0
PC <corno@alum.mit.edu>
2021-04-13 14:36:56 +0100

    Add files for parsing RSS and TheSpoof HTML Add the new sarcasm data

--- src/inputoutput.py
+++ src/inputoutput.py
@@ -5,7 +5,6 @@ import os
 def parse_json(file_name):
     for line in open(file_name, 'r'):
         yield json.loads(line.strip())
-        #yield eval(line)
 
 
 def tokenize(text):
@@ -25,3 +24,24 @@ if __name__ == '__main__':
     x, y = get_data(
         os.path.join('..', 'datasets', 'sarcasm_headlines_dataset.json'))
     print("")
+    keep = []
+    extract = []
+    count = 0
+    for i, (headline, label) in enumerate(zip(x, y)):
+        if count < 50:
+            if int(label) == 1:
+                extract.append({'headline': headline, 'is_sarcastic': label})
+                count += 1
+            else:
+                keep.append({'headline': headline, 'is_sarcastic': label})
+        else:
+            keep.append({'headline': headline, 'is_sarcastic': label})
+    with open('extracted_us.json', 'w') as fh:
+        for item in extract:
+            json.dump(item, fh)
+            fh.write("\n")
+
+    with open('keep_us.json', 'w') as fh:
+        for item in keep:
+            json.dump(item, fh)
+            fh.write("\n")
