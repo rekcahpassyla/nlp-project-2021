ce88b8f320ab42ba8d05e99a96fe17e8f806b338
PC <corno@alum.mit.edu>
2021-04-22 22:51:38 +0100

    Add extra condition

--- scripts/classify.py
+++ scripts/classify.py
@@ -104,7 +104,7 @@ def bulk_eval(
     test_encodings = tokenizer(test_text, truncation=True, padding=True)
 
     min_filename = "%s_%s.pt" % (args_pack["name"], bert_type)
-    if results_dir is not None:
+    if results_dir is not None and not train:
         min_filename = os.path.join(results_dir, min_filename)
     train_dataset = SarcasmDataset(train_encodings, train_labels)
     val_dataset = SarcasmDataset(val_encodings, val_labels)
