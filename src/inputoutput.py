03db7e4f1d53389fa0eaeaa847009d691442f7ce
PC <corno@alum.mit.edu>
2021-04-22 22:37:07 +0100

    lowercase test data

--- src/inputoutput.py
+++ src/inputoutput.py
@@ -21,36 +21,28 @@ def get_data(file_name):
 
 
 if __name__ == '__main__':
-    paths = [
-        os.path.join('..', 'datasets', 'raw_data', 'sarcasm_headlines_dataset_uk.json'),
-        os.path.join('..', 'datasets', 'raw_data', 'sarcasm_headlines_dataset_us.json'),
-        os.path.join('..', 'datasets', 'raw_data', 'sarcasm_headlines_dataset.json'),
-        os.path.join('..', 'datasets', 'train_set_uk.json'),
-        os.path.join('..', 'datasets', 'train_set_us.json'),
-        os.path.join('..', 'datasets', 'train_set_all.json'),
+    basepath = os.path.join('..', 'datasets')
+    files = [
+        'new_test_set',
+        'test_set_uk',
+        'test_set_all',
+        'train_set_uk',
+        'train_set_all',
     ]
-    outdir = os.path.join('..', 'datasets', 'glove')
-    for pth in paths:
+    outdir = basepath
+
+    for fn in files:
+        pth = os.path.join(basepath, f"{fn}.json")
+        out = os.path.join(basepath, f"{fn}2.json")
+        print(f"Processing {pth}")
         x, y = get_data(pth)
-        fn = os.path.basename(pth).replace('json', 'txt')
-        if fn.startswith('sarcasm_headlines_dataset'):
-            fn = fn.replace('sarcasm_headlines_dataset', 'testtrain_set')
-        # for each input file, just stick all the headlines together
-        # in one big string
-        both = []
-        pos = []
-        neg = []
-        for i, (headline, label) in enumerate(zip(x, y)):
-            headline = headline.lower()
-            tokens = headline.split(" ")
-            both += tokens
-            if int(label) == 1:
-                pos += tokens
-            elif int(label) == 0:
-                neg += tokens
-        with open(os.path.join(outdir, fn), 'w') as fh:
-            fh.write(" ".join(both))
-        with open(os.path.join(outdir, f"sarcastic_{fn}"), 'w') as fh:
-            fh.write(" ".join(pos))
-        with open(os.path.join(outdir, f"nonsarcastic_{fn}"), 'w') as fh:
-            fh.write(" ".join(neg))
+
+        with open(out, 'w') as fh:
+            for headline, category in zip(x, y):
+                item = {
+                    "headline": headline.lower(),
+                    "is_sarcastic": category
+                }
+                json.dump(item, fh)
+                fh.write("\n")
+
