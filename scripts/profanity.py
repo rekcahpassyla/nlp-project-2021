df1be5820c5da8e7a6c9e11f26fc3007a6738bd1
PC <corno@alum.mit.edu>
2021-04-28 15:44:52 +0100

    Add more plotting

--- scripts/profanity.py
+++ scripts/profanity.py
@@ -43,4 +43,30 @@ for f in files:
 
 stats = pd.DataFrame(stats)
 
-stats.T
+col_order = ['train_set_us', 'train_set_uk', 'train_set_all',
+             'test_set_us', 'test_set_uk', 'test_set_all',
+             'new_test_set']
+
+
+test = stats.T.unstack(level=1).stack(level=0).unstack(level=1)
+test = test.reindex(col_order)
+
+fig, ax = plt.subplots()
+ax.bar(np.arange(0, 7), yerr=test['non_sarcastic']['std'], align='edge', width=0.2, alpha=0.5, ecolor='#1f77b4', capsize=10, height=test['non_sarcastic']['mean'], label='Non sarcastic', error_kw=dict(capthick=0.1))
+
+# First one must be empty. Don't know why. 
+labels = ['', 'US/Training', 'UK/Training', 'Combined/Training',
+          'US/Test', 'UK/Test', 'Combined/Test',
+          'Adversarial']
+
+#ax.set_xticklabels([''] + [str(item) for item in test.index])
+ax.set_xticklabels(labels)
+plt.xticks(rotation=60)
+#plt.margins(0.2)
+plt.subplots_adjust(bottom=0.3)
+ax.bar(np.arange(0, 7), yerr=test['sarcastic']['std'], align='edge', width=-0.2,  alpha=0.5, ecolor='#ff7f0e', capsize=10, height=test['sarcastic']['mean'], label='Sarcastic', error_kw=dict(capthick=0.1))
+plt.legend()
+plt.grid()
+#plt.title('Mean profanity score for datasets')
+plt.show()
+plt.savefig('profanity_datasets.png')
